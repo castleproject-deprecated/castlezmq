c608cb843e926243dca118b134da333965f85115
hammett <hammett@gmail.com>
2014-06-24 18:32:30 -0300

    Extracted remoterequest in order to unit test it

---
+++ extensions/Castle.Facilities.Zmq.Rpc/Local/RemoteRequestService.cs
@@ -0,0 +1,53 @@
+namespace Castle.Facilities.Zmq.Rpc
+{
+	using System;
+	using Castle.Facilities.Zmq.Rpc.Internal;
+	using Castle.Facilities.Zmq.Rpc.Remote;
+	using Castle.Zmq;
+	using Castle.Zmq.Rpc.Model;
+
+	public class RemoteRequestService
+	{
+		private readonly IZmqContext _context;
+		private readonly RemoteEndpointRegistry _endpointRegistry;
+		private readonly SerializationStrategy _serializationStrategy;
+
+		public RemoteRequestService(IZmqContext context, 
+									RemoteEndpointRegistry endpointRegistry, 
+									SerializationStrategy serializationStrategy)
+		{
+			this._context = context;
+			this._endpointRegistry = endpointRegistry;
+			this._serializationStrategy = serializationStrategy;
+		}
+
+		public object Invoke(string host, string service, string methodName, 
+							 object[] parameters, Type[] parametersTypes, Type retType)
+		{
+			var endpoint = this._endpointRegistry.GetEndpoint(host);
+
+			var serializedArs = this._serializationStrategy.SerializeParameters(parameters, parametersTypes);
+			var serializedPInfo = this._serializationStrategy.SerializeParameterTypes(parametersTypes);
+
+			var requestMessage = new RequestMessage(service, methodName, serializedArs, serializedPInfo);
+
+			var request = new RemoteRequest(this._context, endpoint, requestMessage, this._serializationStrategy);
+			
+			ResponseMessage response = request.Get();
+
+			if (response.ExceptionInfo != null)
+			{
+				var msg = "Remote server or invoker threw " + response.ExceptionInfo.Typename + " with message " +
+				          response.ExceptionInfo.Message;
+				throw new Exception(msg);
+			}
+
+			if (retType != typeof(void))
+			{
+				return Serialization.DeserializeResponseValue(response, retType);
+			}
+
+			return null;
+		}
+	}
+}
---
+++ tests/Castle.Facilities.Zmq.Rpc.Tests/RemoteRequestServiceTestCase.cs
@@ -0,0 +1,72 @@
+﻿namespace Castle.Facilities.Zmq.Rpc.Tests
+{
+	using System;
+	using Castle.Facilities.Zmq.Rpc.Internal;
+	using Castle.Facilities.Zmq.Rpc.Remote;
+	using Castle.Zmq.Rpc.Model;
+	using Castle.Zmq.Stubs;
+	using FluentAssertions;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class RemoteRequestServiceTestCase
+	{
+		private const string TestHostName = "host";
+		private const string TestService = "service";
+
+		private StubContext _context;
+		private RemoteEndpointRegistry _registry;
+		private SerializationStrategy _serialization;
+
+		[SetUp]
+		public void Init()
+		{
+			_registry = new RemoteEndpointRegistry();
+			_serialization = new ProtoBufSerializationStrategy();
+
+			_registry.ReRoute(TestHostName, "endpoint1");
+		}
+
+		[Test]
+		public void Invoke_sends_request_to_right_endpoint()
+		{
+			StubSocket socket = null;
+
+			_context = new StubContext(type =>
+			{
+				socket = new StubSocket(type);
+				socket.ToRecv.Add( Builder.SerializeResponse(new ResponseMessage()) );
+				return socket;
+			});
+
+
+			var req = new RemoteRequestService(_context, _registry, _serialization);
+
+			var result = req.Invoke(TestHostName, TestService, "method", 
+									new object[0], Type.EmptyTypes, typeof (void));
+
+			result.Should().BeNull();
+		}
+
+		[Test]
+		public void Invoke_sends_request_and_restore_result()
+		{
+			StubSocket socket = null;
+
+			_context = new StubContext(type =>
+			{
+				socket = new StubSocket(type);
+				socket.ToRecv.Add(Builder.SerializeResponse( Builder.BuildResponse(new Tuple<object, Type>("hello", typeof(string))) ));
+				return socket;
+			});
+
+
+			var req = new RemoteRequestService(_context, _registry, _serialization);
+
+			var result = req.Invoke(TestHostName, TestService, "method",
+									new object[0], Type.EmptyTypes, typeof(string));
+
+			result.Should().Be("hello");
+		}
+	}
+}
