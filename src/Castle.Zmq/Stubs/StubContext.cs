195599feaa4bf2e963c76956e49c041371b3dc5d
hammett <hammett@gmail.com>
2014-06-24 18:31:39 -0300

    adding StubContext to ease testing

---
+++ src/Castle.Zmq/Stubs/StubContext.cs
@@ -0,0 +1,39 @@
+﻿namespace Castle.Zmq.Stubs
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class StubContext : IZmqContext
+	{
+		private readonly Func<SocketType, StubSocket> _creator = (type) => new StubSocket(type);
+		private bool _disposed;
+
+		public StubContext()
+		{
+			this.LastSocketCreatedByType = new Dictionary<SocketType, StubSocket>();
+		}
+
+		public StubContext(Func<SocketType, StubSocket> creator) : this()
+		{
+			_creator = creator;
+		}
+
+		public IZmqSocket CreateSocket(SocketType type)
+		{
+			if (_disposed) throw new ObjectDisposedException("StubContext disposed");
+
+			var socket = _creator(type);
+
+			this.LastSocketCreatedByType[type] = socket;
+
+			return socket;
+		}
+
+		public Dictionary<SocketType, StubSocket> LastSocketCreatedByType { get; private set; } 
+
+		public void Dispose()
+		{
+			_disposed = true;
+		}
+	}
+}
