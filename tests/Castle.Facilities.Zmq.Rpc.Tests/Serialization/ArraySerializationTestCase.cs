34b7731f72c35482821ec9d43ec7c747a68e3e2b
hammett <hammett@gmail.com>
2014-06-24 18:30:46 -0300

    Adapted to new api surface

--- tests/Castle.Facilities.Zmq.Rpc.Tests/Serialization/ArraySerializationTestCase.cs
+++ tests/Castle.Facilities.Zmq.Rpc.Tests/Serialization/ArraySerializationTestCase.cs
@@ -1,8 +1,10 @@
 ﻿namespace Castle.Facilities.Zmq.Rpc.Tests
 {
+	using System.Linq;
 	using Castle.Facilities.Zmq.Rpc.Internal;
 	using FluentAssertions;
 	using NUnit.Framework;
+	using ProtoBuf;
 
 	[TestFixture]
 	public class ArraySerializationTestCase
@@ -10,9 +12,9 @@
 		[Test]
 		public void Simplest_case_works()
 		{
-			var buffer = Serialization.serialize_array(new[] { "1", "2", "3" });
+			var buffer = Serialization.SerializeArray(new[] { "1", "2", "3" });
 
-			var array = Serialization.deserialize_array(typeof(string[]), buffer);
+			var array = Serialization.DeserializeArray(buffer, typeof(string[]));
 			array.Should().BeOfType<string[]>();
 			var strings = (string[])array;
 			strings.Length.Should().Be(3);
@@ -24,29 +26,29 @@
 		[Test]
 		public void Int32_case_works()
 		{
-			var buffer = Serialization.serialize_array(new[] { 1, 2, 3 });
+			var buffer = Serialization.SerializeArray(new[] { 1, 2, 3 });
 
-			var array = Serialization.deserialize_array(typeof(int[]), buffer);
+			var array = Serialization.DeserializeArray(buffer, typeof(int[]));
 			array.Should().BeOfType<int[]>();
-			//			var ints = (int[])array;
-			//			ints.Length.Should().Be(3);
-			//			ints[0].Should().Be(1);
-			//			ints[1].Should().Be(2);
-			//			ints[2].Should().Be(3);
+			var list = array;
+			list.Count.Should().Be(3);
+			list[0].Should().Be(1);
+			list[1].Should().Be(2);
+			list[2].Should().Be(3);
 		}
 
 		[Test]
 		public void Protocol_case_works()
 		{
-			var buffer = Serialization.serialize_array(new[]
+			var buffer = Serialization.SerializeArray(new[]
 			{
-				new MyCustomClass() { Age = 1, Name = "1" },
-				new MyCustomClass() { Age = 2, Name = "2" }
+				new ParameterSerializationTestCase.MyCustomClass() { Age = 1, Name = "1" },
+				new ParameterSerializationTestCase.MyCustomClass() { Age = 2, Name = "2" }
 			});
 
-			var array = Serialization.deserialize_array(typeof(MyCustomClass[]), buffer);
-			array.Should().BeOfType<MyCustomClass[]>();
-			var myCustomClasses = (MyCustomClass[])array;
+			var array = Serialization.DeserializeArray(buffer, typeof(ParameterSerializationTestCase.MyCustomClass[]));
+			array.Should().BeOfType<ParameterSerializationTestCase.MyCustomClass[]>();
+			var myCustomClasses = (ParameterSerializationTestCase.MyCustomClass[])array;
 			myCustomClasses.Length.Should().Be(2);
 			myCustomClasses[0].Age.Should().Be(1);
 			myCustomClasses[1].Age.Should().Be(2);
@@ -57,20 +59,41 @@
 		[Test]
 		public void Protocol_pseudo_inheritance_case_works()
 		{
-			var buffer = Serialization.serialize_array(new Base[]
+			var buffer = Serialization.SerializeArray(new Base[]
 			{
 				new Derived1() { Something = 2, DerivedProp1 = 1 },
 				new Derived2() { Something = 10, DerivedProp2 = "1" },
 				new Derived1() { Something = 3, DerivedProp1 = 1000 },
 			});
 
-			var array = Serialization.deserialize_array(typeof(Base[]), buffer);
+			var array = Serialization.DeserializeArray(buffer, typeof(Base[]));
 			array.Should().BeOfType<Base[]>();
 			var myCustomClasses = (Base[])array;
 			myCustomClasses.Length.Should().Be(3);
 			myCustomClasses[0].Should().BeOfType<Derived1>();
 			myCustomClasses[1].Should().BeOfType<Derived2>();
 			myCustomClasses[2].Should().BeOfType<Derived1>();
+		}
+
+		[ProtoContract]
+		[ProtoInclude(1, typeof(Derived1))]
+		[ProtoInclude(2, typeof(Derived2))]
+		public class Base
+		{
+			[ProtoMember(10)]
+			public int Something { get; set; }
+		}
+		[ProtoContract]
+		public class Derived1 : Base
+		{
+			[ProtoMember(20)]
+			public int DerivedProp1 { get; set; }
+		}
+		[ProtoContract]
+		public class Derived2 : Base
+		{
+			[ProtoMember(30)]
+			public string DerivedProp2 { get; set; }
 		}
 	}
 }
