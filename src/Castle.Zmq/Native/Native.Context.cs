9c3f3ff9044d7e388a44beb4d73efa4fb64b3265
hammett <hammett@gmail.com>
2014-07-01 18:53:08 -0300

    minor change - split file

--- src/Castle.Zmq/Native/Native.Context.cs
+++ src/Castle.Zmq/Native/Native.Context.cs
@@ -5,15 +5,6 @@
 
 	internal static partial class Native
 	{
-		internal static class Device
-		{
-			[DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
-			public static extern int zmq_proxy(IntPtr front, IntPtr back, IntPtr capture);
-		}
-	}
-
-	internal static partial class Native
-	{
 		internal static class Context
 		{
 			[DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
---
+++ src/Castle.Zmq/Native/Native.Device.cs
@@ -0,0 +1,14 @@
+﻿namespace Castle.Zmq
+{
+	using System;
+	using System.Runtime.InteropServices;
+
+	internal static partial class Native
+	{
+		internal static class Device
+		{
+			[DllImport("libzmq", CallingConvention = CallingConvention.Cdecl)]
+			public static extern int zmq_proxy(IntPtr front, IntPtr back, IntPtr capture);
+		}
+	}
+}
